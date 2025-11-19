777fd6266553852c76650b72fd599ad4e037932d
JINLING1 <1124290657@qq.com>
2025-11-19 21:10:21 +0800

    feat: implement detailed content for ScoreList view

--- src/features/score/ScoreList.vue
+++ src/features/score/ScoreList.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    ScoreList
+  <div class="overflow-x-auto">
+    <table class="table table-lg">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Class</th>
+          <th>Subject</th>
+          <th>Semester</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Cy Ganderton</td>
+          <td>Class 1 | Year 9</td>
+          <td>Math</td>
+          <td>2025 Spring</td>
+          <td>86</td>
+          <td>
+            <button class="btn btn-ghost btn-sm">details</button>
+            <button class="btn btn-error btn-sm">delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
