a7054f7858e74ed9ee85de0525189a8390d9a01a
JINLING1 <1124290657@qq.com>
2025-11-19 21:01:57 +0800

    feat: implement StudentList with StudentListItem
    Introduce StudentListItem for displaying a single student row. StudentList now renders a list of
    students using StudentListItem components.

--- src/features/student/StudentList.vue
+++ src/features/student/StudentList.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
-    StudentList
+  <div class="overflow-x-auto">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th>
+            <label>
+              <input type="checkbox" class="checkbox" />
+            </label>
+          </th>
+          <th>Name</th>
+          <th>Class</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <StudentListItem />
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup>
-
+import StudentListItem from './StudentListItem.vue'
 </script>
---
+++ src/features/student/StudentListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <tr>
+    <th>
+      <label>
+        <input type="checkbox" class="checkbox" />
+      </label>
+    </th>
+    <td>
+      <div class="flex items-center gap-3">
+        <div class="avatar">
+          <div class="mask mask-squircle h-12 w-12">
+            <img src="https://img.daisyui.com/images/profile/demo/2@94.webp" alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+        <!-- name and gender -->
+        <div>
+          <div class="font-bold">Hart Hagerty</div>
+          <div class="text-sm opacity-50">male</div>
+        </div>
+      </div>
+    </td>
+    <!-- class and teacher -->
+    <td>
+      Class 1 | Year 9
+      <br />
+      <span class="badge badge-ghost badge-sm">JinLing</span>
+    </td>
+    <th>
+      <button class="btn btn-ghost btn-xs">details</button>
+      <button class="btn btn-error btn-xs">delete</button>
+    </th>
+  </tr>
+</template>
+
+<script setup>
+
+</script>
