60e29d87240a9853cb1b7f196e431f80f48bce9a
JINLING1 <1124290657@qq.com>
2026-03-30 18:23:11 +0800

    style: improve layout and styling of the toolbar component

--- src/ui/ToolBar.vue
+++ src/ui/ToolBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="my-4 grid  grid-cols-4 gap-2">
-    <section class=" mx-auto col-span-1">
+  <div class="my-4 flex flex-col md:grid md:grid-cols-4 gap-4 md:gap-2 items-center">
+    <section class="flex flex-wrap gap-2 justify-center w-full md:col-span-1">
       <Condition v-for="(condition, idx) in studentSearchCondition" :onDelete='() => onDelete(idx)' :key="idx"
         v-if="isStudentList">
         {{ condition }}
@@ -10,22 +10,24 @@
         {{ condition }}
       </Condition>
     </section>
-    <div class="col-span-2 mx-auto">
-      <label class="input w-md">
-        <svg class="h-[1em] opacity-50 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-          @click="onSearch">
-          <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </g>
-        </svg>
-        <input type="search" required placeholder="Search" v-model="searchString" />
-      </label>
-    </div>
-    <div class="col-span-1 text-center">
-      <button class="btn btn-outline btn-primary  btn-md" @click="onClick" v-if="!isStudent">
-        {{ route.name === 'score' ? 'Upload Score' : 'Add Student' }}
-      </button>
+    <div class="w-full md:col-span-3 flex flex-row items-center gap-2 px-2 md:px-0 md:grid md:grid-cols-3">
+      <div class="flex-grow md:col-span-2 flex md:justify-center">
+        <label class="input w-full md:w-md">
+          <svg class="h-[1em] opacity-50 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+            @click="onSearch">
+            <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" required placeholder="Search" v-model="searchString" class="grow w-full" />
+        </label>
+      </div>
+      <div class="shrink-0 md:col-span-1 flex md:justify-center">
+        <button class="btn btn-outline btn-primary btn-md w-full md:w-auto" @click="onClick" v-if="!isStudent">
+          {{ route.name === 'score' ? 'Upload Score' : 'Add Student' }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
